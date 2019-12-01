e46a7e48f9288d827355b73a2719e2296b987870
Sean King <sean@seking.dev>
2019-12-01 15:44:35 +0100

    feat(ipfs upload): addin upload flow with ipfs

--- src/upload/components/upload-drop-zone/index.js
+++ src/upload/components/upload-drop-zone/index.js
@@ -1,10 +1,11 @@
 // Components
 import React, { useCallback, useState, Fragment } from "react";
 import { Upload as UploadIcon } from "react-feather";
-import { Check } from "react-feather";
+import { Lock } from "react-feather";
 import { useDropzone } from "react-dropzone";
 import useOnDrop from "./utils/use-on-drop";
 import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 // Styling
 import "./upload-drop-zone.css";
@@ -12,31 +13,57 @@ import "./upload-drop-zone.css";
 const UploadDropZone = () => {
   const [isUploaded, setIsUploaded] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
-  const onDrop = useOnDrop(setIsUploaded);
+  const [ipfsHash, setIpfsHash] = useState("");
+
+  const onDrop = useOnDrop(setIsUploaded, setIsUploading, setIpfsHash);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-  return (
-    <>
-      {isUploaded ? (
-        <Grid container direction="column" justify="center" alignItems="center">
-          <Grid item>
-            <Check size={120} />
-          </Grid>
-          <Grid item>
-            <p>Uploaded</p>
+  if (isUploading)
+    return (
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <CircularProgress className="progress" size={110} color="inherit" />
+        </Grid>
+        <Grid item>
+          <p>Securing files in vault...</p>
+        </Grid>
+      </Grid>
+    );
+
+  if (isUploaded)
+    return (
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <Lock size={120} />
+        </Grid>
+        <Grid item>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid>
+              <p>FIles added to vault </p>
+            </Grid>
+            <Grid>
+              <h1>{ipfsHash}</h1>
+            </Grid>
           </Grid>
         </Grid>
-      ) : (
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <UploadIcon size={120} />
-          )}
-        </div>
-      )}
-    </>
-  );
+      </Grid>
+    );
+
+  if (!isUploaded && !isUploading)
+    return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <UploadIcon size={120} />
+        )}
+      </div>
+    );
 };
 export default UploadDropZone;
--- src/upload/components/upload-drop-zone/utils/use-on-drop.js
+++ src/upload/components/upload-drop-zone/utils/use-on-drop.js
@@ -1,22 +1,36 @@
 import React, { useCallback } from "react";
+import ipfsClient from "ipfs-http-client";
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https"
+});
 
-const useOnDrop = hook =>
+const useOnDrop = (setIsUploaded, setIsUploading, setIpfsHash) =>
   useCallback(acceptedFiles => {
     console.log("starting");
-
+    setIsUploading(true);
+    let files = [];
     acceptedFiles.forEach(file => {
       const reader = new FileReader();
       // console.log(file);
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        // Do whatever you want with the file contents
-        const binaryStr = reader.result;
-        // console.log(binaryStr);
+        files.push({
+          path: file.path,
+          content: reader.result
+        });
+        ipfs.add(reader.result, (err, result) => {
+          console.log(err, result);
+          setIsUploading(false);
+          setIpfsHash(result[0].hash);
+        });
       };
+      console.log(file);
       reader.readAsArrayBuffer(file);
     });
-    hook(true);
+    setIsUploaded(true);
     console.log("finishing");
   }, []);
 
